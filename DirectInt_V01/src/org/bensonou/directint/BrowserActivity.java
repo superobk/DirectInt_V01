418beb30a088715395995a3e3b2363e3923191a0
superobk <umwcl.kai@gmail.com>
2014-10-08 01:31:26 +0800

    Signed-off-by: superobk <umwcl.kai@gmail.com>

---
+++ DirectInt_V01/src/org/bensonou/directint/BrowserActivity.java
@@ -0,0 +1,33 @@
+package org.bensonou.directint;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+
+public class BrowserActivity extends Activity {
+
+	WebView browser;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_browser);
+		
+		browser = new WebView(this);
+		browser.getSettings().setJavaScriptEnabled(true); 
+		browser.loadUrl("http://www.baidu.com/"); 
+
+        setContentView(browser); 	
+	}
+	
+	@Override
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) { 
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && browser.canGoBack()) { 
+        	browser.goBack();
+            return true; 
+        } 
+        return false; 
+	}
+}
--- DirectInt_V01/src/org/bensonou/directint/MainActivity.java
+++ DirectInt_V01/src/org/bensonou/directint/MainActivity.java
@@ -4,18 +4,28 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.*;
+
 import org.bensonou.directint.util.*;
 
 public class MainActivity extends Activity{
 
 	WifiUtility myWifi;
 	
+	ListView listWifi;
+	SimpleAdapter adapterListWifi;
+	
+	Button btnLoad;
+	Button btnRefresh;
+	
 	private void initialComponent() {
+		myWifi = new WifiUtility(this);
+		
 		
 	}
 	
 	private void initailVariable() {
-		myWifi = new WifiUtility(this);
+		
 	}
 	
 	@Override
@@ -23,8 +33,12 @@ public class MainActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		
+		
 	}
 
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
